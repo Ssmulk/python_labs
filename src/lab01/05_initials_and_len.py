684ce9f71c1515aafb81d32aedcee8c6b494a536
Алиса <ssmulk23@mail.ru>
2026-09-25 16:35:23 +0300

    ex05

--- src/lab01/05_initials_and_len.py
+++ src/lab01/05_initials_and_len.py
@@ -1,7 +1,6 @@
 fio=input('ФИО: ')
 parts=fio.split()
 initials=''.join(word[0] for word in parts).upper()+'.'
-clean_fio=''.join(parts)
 
 print(f'Инициалы: {initials}')
 print(f'Длина (символов): {len(fio)}')
